075febe5f321a653cb8b2ed31d09525aa0a78559
Timo Reichl <backraw@gmx.de>
2017-03-08 01:46:57 +0100

    main.cpp: Run serverlist.py if necessary
    Signed-off-by: Timo Reichl <backraw@gmx.de>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
+
 #include <QApplication>
+#include <QProcess>
 
 #include "paths.h"
 
@@ -10,6 +12,15 @@ int main(int argc, char *argv[])
     if (!steamcmd::PATH_SETTINGS.exists())
     {
         steamcmd::PATH_CONFIG.mkdir(steamcmd::PATH_SETTINGS.dirName());
+    }
+
+    // Parse https://developer.valvesoftware.com/wiki/Dedicated_Servers_List if necessary
+    if (!steamcmd::PATH_SERVERLIST_JSON.exists())
+    {
+        QProcess serverlist;
+
+        serverlist.start("python3 ./serverlist.py");
+        serverlist.waitForFinished();
     }
 
     // Show the main window
