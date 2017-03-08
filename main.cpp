24b903d5b583ff81a8b93359ebe89a510fe5e7b7
Timo Reichl <backraw@gmx.de>
2017-03-08 01:26:33 +0100

    main.cpp: Create directory ~/.config/steamcmd-gui-qt if necessary
    Signed-off-by: Timo Reichl <backraw@gmx.de>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include "paths.h"
+
+
 int main(int argc, char *argv[])
 {
+    // Create directory ~/.config/steamcmd-gui-qt if necessary
+    if (!steamcmd::PATH_SETTINGS.exists())
+    {
+        steamcmd::PATH_CONFIG.mkdir(steamcmd::PATH_SETTINGS.dirName());
+    }
+
     // Show the main window
     QApplication a(argc, argv);
     MainWindow w;
